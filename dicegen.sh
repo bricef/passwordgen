c3d494d2cc6695bdf36ed541899c3b8c6bb9856e
Brice Fernandes <brice@weave.works>
2018-01-23 20:35:37 +0000

    Add diceware word generator

---
+++ dicegen.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DICELIST_DIR="./Dicelists"
+
+function get_random_line {
+  local FILE=$(shuf -n 1 ${DICELIST_DIR}/metalist.txt )
+  # echo $FILE 1>&2
+  local LINES=$(cat ${DICELIST_DIR}/${FILE})
+  # echo $LINES 1>&2
+  local LINE=$(echo "${LINES}" | shuf -n 1)
+  echo $LINE
+}
+
+
+function get_word_from_line {
+  echo $(shuf -n 1 -e $( echo $(get_random_line) | sed -e 's,\s+, ,g' | cut -d ' ' -f 2-))
+}
+
+NWORDS=$1
+
+OUTPUT=""
+for i in $(seq 1 $NWORDS)
+do
+  OUTPUT="$(get_word_from_line)\n$OUTPUT"
+done
+
+echo -ne $OUTPUT
